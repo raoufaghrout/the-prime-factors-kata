b5f7afa6cf843025d859b97eb91ce9fde374c975
Raouf Aghrout <aghrout.raouf@gmail.com>
2015-08-12 23:21:38 +0100

    added first test

--- src/main/java/com/butunclebob/PrimeFactors.java
+++ src/main/java/com/butunclebob/PrimeFactors.java
@@ -1,5 +1,11 @@
 package com.butunclebob;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
 
+    public static List<Integer> generate(int n) {
+        return new ArrayList<Integer>();
+    }
 }
--- src/test/java/com/butunclebob/PrimeFactorsTest.java
+++ src/test/java/com/butunclebob/PrimeFactorsTest.java
@@ -1,5 +1,21 @@
 package com.butunclebob;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class PrimeFactorsTest {
 
+    @Test
+    public void shouldGenerateOne() {
+        assertThat(PrimeFactors.generate(1), is(list()));
+    }
+
+    private List<Integer> list() {
+        return new ArrayList<Integer>();
+    }
 }
